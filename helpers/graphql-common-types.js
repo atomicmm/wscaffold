8adf916e769da33221daa08b687c496f4999fe3b
Atomic <atomicmm@gmail.com>
2017-02-22 16:58:28 +0800

    add graphql common types

---
+++ helpers/graphql-common-types.js
@@ -0,0 +1,34 @@
+//标准返回的对象
+const ObjectType = `
+    type Object {
+        id: ID! obj: String
+    }
+`
+
+//分页对象
+const PageType = `
+    type Page {
+        totalNum:Int! pageSize:Int! countPage:Int! currentPage:Int! items:[Object]
+    }
+`
+
+//标准输入
+const ObjectInputType = `
+    input ObjectInput {
+        payload: String
+    }
+`
+
+//分页查询
+const PageQueryInputType = `
+    input PageQueryInput {
+        cond: String offset: Int limit: Int
+    }
+`
+
+module.exports = {
+    ObjectType,
+    PageType,
+    ObjectInputType,
+    PageQueryInputType
+}
